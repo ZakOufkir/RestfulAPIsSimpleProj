7e6d02c25154d6daa102245024075edb6db2de67
Zakaria Ait oufkir <aitoufkirzakaria@gmail.com>
2025-05-13 19:00:05 -0500

    formatting timestamp

--- src/main/java/com/examplerest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/examplerest/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,17 +18,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String,Object> >handleNotFound(ResourceNotFoundException ex){
         Map<String,Object> error = new HashMap<>();
-        error.put("timestamp", LocalDateTime.now());
+        error.put("timestamp", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
         error.put("error","Not Found");
+        error.put("error Code",HttpStatus.NOT_FOUND.value());
         error.put("message",ex.getMessage());
+
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(IllegalArgumentsException.class)
     public ResponseEntity<Map<String,Object>> handleBadRequest(IllegalArgumentsException ex){
         Map<String,Object> error = new HashMap<>();
-        error.put("timestamp", LocalDateTime.now());
+        error.put("timestamp", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
         error.put("error","Invalid Request");
+        error.put("error Code",HttpStatus.BAD_REQUEST.value());
         error.put("message",ex.getMessage());
+
         return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
     }
 }
